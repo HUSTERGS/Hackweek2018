f61bebc8e512a406303bb15c3abb01f904e784db
HUSTGS <1148690954@qq.com>
2018-08-05 16:46:36 +0800

    learning session in flask

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, flash, session, redirect, url_for
 import mysql.connector
 
 app = Flask(__name__)
+app.secret_key = 'hackweek2018'
 
 con = mysql.connector.connect(
     user='root', password='MySQL#232', database='hackweek2018', port=3305, use_unicode=True)
@@ -22,18 +23,40 @@ def register():
 def userlogin():
     useremail = request.form.get('useremail')
     password = request.form.get('password')
-    print(useremail, password)
     cursor.execute(
         "select * from users where useremail = %s and userpw = %s", (useremail, password))
-    if (cursor.fetchall() == []):
+    result = cursor.fetchall()
+    if (result == []):
         return '用户不存在或密码错误'
+        # flash('用户名不存在或密码错误')
+        # return render_template('login.html')
     else:
-        return '登录成功'
+        print(result[0][0])
+        session['login_user'] = result[0][0]
+        return redirect(url_for('homepage'))
 
 
 @app.route('/home', methods=['GET'])  # response the homepage request
 def homepage():
     return render_template('index.html')
+
+
+@app.route('/ex', methods=['GET'])
+def ex():
+    flash('cdas')
+    return 'cdsacdsacdsa'
+
+
+@app.route('/logout')
+def logout():
+    session.pop('login_user', None)
+    return redirect(url_for('homepage'))
+
+
+@app.context_processor
+def my_context_processor():
+    login_user = session.get('login_user', '')
+    return {'login_user': login_user}
 
 
 if __name__ == '__main__':
